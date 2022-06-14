3e765bf91b9c6e1a25d34742f21a1fa2593861bf
Barbara Rodrigues <barbara.rodrigues@fmxsolucoes.com.br>
2022-06-13 23:06:18 -0300

    get, post, put delete

--- backend1/controllers/productController.js
+++ backend1/controllers/productController.js
@@ -1,3 +1,61 @@
-exports.getAllProducts = (req, res)=>{
-    res.status(200).json({message: "Route is working"})
+const Product = require("../models/productModel");
+
+//Criacao do produto - admin
+exports.createProduct = async (req, res, next) => {
+    const product = await Product.create(req.body);
+
+    res.status(201).json({
+        success: true,
+        product
+    })
+}
+
+// buscar todos os produtos 
+exports.getAllProducts = async (req, res) => {
+    const products = await Product.find();
+
+    res.status(200).json({ success: true, products })
+}
+
+// atualizar um produto - admin
+exports.updateProduct = async (req, res, next) => {
+    let product = await Product.findById(req.params.id);
+
+    if (!product) {
+        return res.status(500).json({
+            sucess: false,
+            message: "Produto não encontrado"
+        })
+    }
+
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    });
+
+    res.status(200).json({
+        success: true,
+        product
+    })
+}
+
+//Excluir um produto
+
+exports.deleteProduct = async(req, res, next) =>{
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+        return res.status(500).json({
+            sucess: false,
+            message: "Produto não encontrado"
+        })
+    }
+
+    await product.remove();
+
+    res.status(200).json({
+        success: true,
+        message: "Produto excluido com sucesso!"
+    })
 }
---
+++ backend1/models/productModel.js
@@ -0,0 +1,72 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Por favor, insira o nome do produto"],
+        trim: true
+    },
+    description: {
+        type: String,
+        required: [true, "Por favor, insira a descrição do produto"]
+
+    },
+    price: {
+        type: String,
+        required: [true, "Por favor, insira o preço do produto"],
+        maxlength: [8, "O preço não pode passar de 8 caracteres"]
+
+    },
+    rating: {
+        type: Number,
+        default: 0
+    },
+    images: [{
+        public_id: {
+            type: String,
+            required: true
+        },
+        url: {
+            type: String,
+            required: true
+        }
+    }],
+    category: {
+        type: String,
+        required: [true, "Por favor, insira a categoria do produto"]
+    },
+    Stock: {
+        type: Number,
+        required: [true, "Por favor, insira o estoque do produto"],
+        maxlength: [4, "O estoque não pode passar de 4 caracteres"],
+        default: 1
+    },
+    //avaliacoes
+
+    numOfReviews: {
+        type: Number,
+        default: 0
+    },
+    reviews: [
+        {
+            name: {
+                type: String,
+                required: true
+            },
+            rating: {
+                type: String,
+                required: true
+            },
+            comment: {
+                type: String,
+                required: true
+            }
+        }
+    ],
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.exports = mongoose.model("Product", productSchema);
