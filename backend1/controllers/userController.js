15baed32d7afad48646d0a949b5a7ce542ede98b
Barbara Rodrigues <barbara.rodrigues@fmxsolucoes.com.br>
2022-06-17 12:31:40 -0300

    forgot, reset and erros - password

--- backend1/controllers/userController.js
+++ backend1/controllers/userController.js
@@ -1,54 +1,123 @@
-const ErrorHander = require("../utils/errorHander");
-const catchAsyncErrors = require("../middleware/catchAsyncErrors");
-const User = require("../models/userModel");
-const sendToken = require("../utils/jwtToken");
+const ErrorHander = require('../utils/errorHander');
+const catchAsyncErrors = require('../middleware/catchAsyncErrors');
+const User = require('../models/userModel');
+const sendToken = require('../utils/jwtToken');
+const sendEmail = require('../utils/sendEmail');
+const crypto = require('crypto');
 
 //Registrando um usuário
 exports.registerUser = catchAsyncErrors(async (req, res, next) => {
+	const { name, email, password } = req.body;
 
-  const { name, email, password } = req.body;
+	const user = await User.create({
+		name,
+		email,
+		password,
+		avatar: {
+			public_id: 'asasd',
+			url: 'asasd',
+		},
+	});
 
-  const user = await User.create({
-    name,
-    email,
-    password,
-    avatar: {
-      public_id: "asasd",
-      url: "asasd",
-    },
-  });
-
-  sendToken(user, 200, res);
+	sendToken(user, 200, res);
 });
-
 
 //Login usuario
 exports.loginUser = catchAsyncErrors(async (req, res, next) => {
+	const { email, password } = req.body;
 
-  const { email, password } = req.body;
+	//verificacao de senha/email usuario
+	if (!email || !password) {
+		return next(new ErrorHander('Por favor, insira um email e uma senha', 400));
+	}
 
-  //verificacao de senha/email usuario
-  if (!email || !password) {
-    return next(new ErrorHander("Por favor, insira um email e uma senha", 400))
-  }
+	const user = await User.findOne({ email }).select('+password');
 
-  const user = await User.findOne({ email }).select("+password");
+	if (!user) {
+		return next(new ErrorHander('Email ou senha invalido', 401));
+	}
 
-  if (!user) {
-    return next(new ErrorHander("Email ou senha invalido", 401))
-  }
+	const isPasswordMatched = await user.comparePassword(password);
 
-  const isPasswordMatched = await user.comparePassword(password);
+	if (!isPasswordMatched) {
+		return next(new ErrorHander('Senha invalida', 401));
+	}
 
-  if (!isPasswordMatched) {
-    return next(new ErrorHander("Senha invalida", 401))
-  }
-
-  sendToken(user, 200, res);
-
-
+	sendToken(user, 200, res);
 });
 
+//logout
+exports.logoutUser = catchAsyncErrors(async (req, res, next) => {
+	res.cookie('token', null, {
+		expires: new Date(Date.now()),
+		httpOnly: true,
+	});
 
+	res.status(200).json({
+		success: true,
+		message: 'Deslogado',
+	});
+});
 
+//recuperação de senha -- envio de email de recuperação
+exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
+	const user = await User.findOne({ email: req.body.email });
 
+	if (!user) {
+		return next(new ErrorHander('Usuário não encontrado', 404));
+	}
+
+	const resetToken = user.getResetPasswordToken();
+
+	await user.save({ validateBeforeSave: false });
+
+	const resetPasswordUrl = `${req.protocol}://${req.get('host')}/password/reset/${resetToken}`;
+
+	const message = `O seu token de recuperação de senha é: - \n\n ${resetPasswordUrl}`;
+
+	try {
+		await sendEmail({
+			email: user.email,
+			subject: `Ecommerce Password Recovery`,
+			message,
+		});
+
+		res.status(200).json({
+			success: true,
+			message: `Email sent to ${user.email} successfully`,
+		});
+	} catch (error) {
+		user.resetPasswordToken = undefined;
+		user.resetPasswordExpire = undefined;
+
+		await user.save({ validateBeforeSave: false });
+
+		return next(new ErrorHander(error.message, 500));
+	}
+});
+
+//recuperação de senha -- troca de senha
+exports.resetPassword = catchAsyncErrors(async (req, res, next) => {
+	const resetPasswordToken = crypto.createHash('sha256').update(req.params.token).digest('hex');
+
+	const user = await User.findOne({
+		resetPasswordToken,
+		resetPasswordExpire: { $gt: Date.now() },
+	});
+
+	if (!user) {
+		return next(new ErrorHander('O token é inválido ou expirou', 400));
+	}
+
+	if (req.body.password !== req.body.confirmPassword) {
+		return next(new ErrorHander('Password does not password', 400));
+	}
+
+	user.password = req.body.password;
+	user.resetPasswordToken = undefined;
+	user.resetPasswordExpire = undefined;
+
+	await user.save();
+
+	sendToken(user, 200, res);
+});
--- backend1/models/userModel.js
+++ backend1/models/userModel.js
@@ -1,67 +1,77 @@
-const mongoose = require("mongoose");
-const validator = require("validator");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, "Por favor, insira o seu nome"],
-        maxLength: [60, "O nome não pode passar de 60 caracteres"],
-        minLength: [3, "O nome não pode ter menos de 3 caracteres"],
-    },
-    email: {
-        type: String,
-        required: [true, "Por favor, insira o seu email"],
-        unique: true,
-        validate: [validator.isEmail, "Por favor, insira um email valido."]
-    },
-    password: {
-        type: String,
-        required: [true, "Por favor, insira a sua senha"],
-        minLength: [4, "A senha não pode ter menos de 3 caracteres"],
-        select: false
-    },
-    avatar: {
-        public_id: {
-            type: String,
-            required: true
-        },
-        url: {
-            type: String,
-            required: true
-        }
-    },
-    role: {
-        type: String,
-        default: "user"
-    },
-    resetPasswordToken: String,
-    resetPasswordExpire: Date,
+	name: {
+		type: String,
+		required: [true, 'Por favor, insira o seu nome'],
+		maxLength: [60, 'O nome não pode passar de 60 caracteres'],
+		minLength: [3, 'O nome não pode ter menos de 3 caracteres'],
+	},
+	email: {
+		type: String,
+		required: [true, 'Por favor, insira o seu email'],
+		unique: true,
+		validate: [validator.isEmail, 'Por favor, insira um email valido.'],
+	},
+	password: {
+		type: String,
+		required: [true, 'Por favor, insira a sua senha'],
+		minLength: [4, 'A senha não pode ter menos de 3 caracteres'],
+		select: false,
+	},
+	avatar: {
+		public_id: {
+			type: String,
+			required: true,
+		},
+		url: {
+			type: String,
+			required: true,
+		},
+	},
+	role: {
+		type: String,
+		default: 'user',
+	},
+	resetPasswordToken: String,
+	resetPasswordExpire: Date,
 });
 
+userSchema.pre('save', async function (next) {
+	if (!this.isModified('password')) {
+		next();
+	}
 
-userSchema.pre("save", async function(next){
-
-    if(!this.isModified("password")){
-        next();
-    }
-
-    //criptografia da senha
-    this.password = await bcrypt.hash(this.password, 10);
+	//criptografia da senha
+	this.password = await bcrypt.hash(this.password, 10);
 });
 
 //JWT TOKEN
-userSchema.methods.getJWTToken = function(){
-    return jwt.sign({id:this._id}, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRE
-    });
+userSchema.methods.getJWTToken = function () {
+	return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+		expiresIn: process.env.JWT_EXPIRE,
+	});
 };
 
 //comparacao senha (criptografada)
 userSchema.methods.comparePassword = async function (password) {
-    return await bcrypt.compare(password, this.password);
-  };
-  
+	return await bcrypt.compare(password, this.password);
+};
 
-module.exports = mongoose.model("User", userSchema);
+userSchema.methods.getResetPasswordToken = function () {
+	// Generating Token
+	const resetToken = crypto.randomBytes(20).toString('hex');
+
+	// Hashing and adding resetPasswordToken to userSchema
+	this.resetPasswordToken = crypto.createHash('sha256').update(resetToken).digest('hex');
+
+	this.resetPasswordExpire = Date.now() + 15 * 60 * 1000;
+
+	return resetToken;
+};
+
+module.exports = mongoose.model('User', userSchema);
