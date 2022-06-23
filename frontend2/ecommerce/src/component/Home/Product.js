d185a588087d2f89a4e75f7eb47ac3e581904d43
Barbara Rodrigues <barbara.rodrigues@fmxsolucoes.com.br>
2022-06-22 23:59:35 -0300

    reducers, actions and api integration in home.js

--- frontend2/ecommerce/src/component/Home/Product.js
+++ frontend2/ecommerce/src/component/Home/Product.js
@@ -11,15 +11,20 @@ const ProductCard = ({ product }) => {
     edit: false,
     activeColor: "#7d344a",
     size: window.innerHeight < 600 ? 20 : 25,
-    value: 2.5,
+    value: product.ratings,
     isHalf: true,
   };
+
   return (
     <Link className="productCard" to={product._id}>
       <img src={product.images[0].url} alt={product.name} />
       <p>{product.name}</p>
       <div>
-        <ReactStars {...options} /> <span>(500 reviews)</span>
+        <ReactStars {...options} />{" "}
+        <span className="productCardSpan">
+          {" "}
+          ({product.numOfReviews} Reviews)
+        </span>
       </div>
       <span>{`R$${product.price}`}</span>
     </Link>
